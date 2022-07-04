42f95ffc22a4c3bc0fa1d4673fa9258193c20399
Mouhamed <mouhamed-el-moustapha.mboup@epitech.eu>
2022-07-04 13:25:35 +0200

    Taille des variables en memoire

--- variables/main.c
+++ variables/main.c
@@ -4,18 +4,20 @@
 int main()
 {
     int mon_int = 14;
-    printf("Mon int = %d \n", mon_int);
+    int taille_int = sizeof(int);
+
+    printf("Mon int = %d et prend en memoire %d octets \n", mon_int, taille_int);
 
 
     char mon_char = 'A';
-    printf("Mon char = %c \n", mon_char);
+    printf("Mon char = %c et prend en memoire %d \n", mon_char, sizeof(char));
 
 
     float mon_float = 0.5;
-    printf("Mon float = %f \n", mon_float);
+    printf("Mon float = %f et prend en memoire %d \n", mon_float, sizeof(float));
 
     double mon_double = 14.6;
-    printf("Mon double = %f \n", mon_double);
+    printf("Mon double = %f et prend en memoire %d \n", mon_double, sizeof(double));
 
     return 0;
 }
