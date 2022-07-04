5b285dba385aa546ca812b5bceb83b691fe999b8
Mouhamed <mouhamed-el-moustapha.mboup@epitech.eu>
2022-07-04 23:07:45 +0200

    Les boucles

---
+++ les boucles/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /* int compteur = 20;
+    do {
+           printf("%d\n", compteur);
+           compteur--;
+        }
+
+    while(compteur != 0);
+
+    printf("%d\n", compteur);
+    return 0; */
+
+
+for (int compteur = 10 ; compteur >= 0 ; compteur--)
+{
+    printf("%d\n", compteur);
+}
+return 0;
+};
