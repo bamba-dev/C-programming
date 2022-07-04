ed02f5098e1dbaddcebc96483b23acff4eda1a35
Mouhamed <mouhamed-el-moustapha.mboup@epitech.eu>
2022-07-04 11:00:44 +0200

    first commit

---
+++ hello_world/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Coucou !\n");
+    printf("\t Aprendre a coder en C");
+    return 0;
+}
---
+++ tp1_syntaxe_et_structure/main.c
@@ -0,0 +1,11 @@
+//Importation des librairies
+#include <stdio.h>
+#include <stdlib.h>
+
+    //Main est la fonction principale
+    int main() {
+
+    printf("TP 1, \"Syntaxe et structure\" Hello World!");
+
+    return 0;
+    }
