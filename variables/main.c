edc98d73c57513b2a9d0cb937f0ffc19af4df43e
Mouhamed <mouhamed-el-moustapha.mboup@epitech.eu>
2022-07-04 11:58:32 +0200

    Les types de variables

--- variables/main.c
+++ variables/main.c
@@ -3,9 +3,19 @@
 
 int main()
 {
-    int taille = 195;
-    int age = 19;
+    int mon_int = 14;
+    printf("Mon int = %d \n", mon_int);
 
-    printf("Vous avez %d ans et vous mesurez %d cm. \n", age, taille);
+
+    char mon_char = 'A';
+    printf("Mon char = %c \n", mon_char);
+
+
+    float mon_float = 0.5;
+    printf("Mon float = %f \n", mon_float);
+
+    double mon_double = 14.6;
+    printf("Mon double = %f \n", mon_double);
+
     return 0;
 }
