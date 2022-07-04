b518f432b19585fab86e587cea75f7878e184e0d
Mouhamed <mouhamed-el-moustapha.mboup@epitech.eu>
2022-07-04 15:47:40 +0200

    Calculs

--- tp2_memoire_et_variable/main.c
+++ tp2_memoire_et_variable/main.c
@@ -1,15 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()
+int main(int argc, char *argv[])
 {
-    printf("TP2: Memoire et Variables\n");
-    int my_int = 20;
-    short my_short = 8;
-    long my_long = 16;
-    float my_float = 5.4;
-    double my_double = 9.7;
-    char my_char = 'B';
-    printf("my_int = %d \n my_short = %d \n my_long = %ld \n my_float = %f \n my_double = %f \n my_char = %c \n", my_int, my_short, my_long, my_float, my_double, my_char);
-    return 0;
+    int resultb = 0;
+    resultb = 14 + 5;
+    printf("%d\n", resultb);
+    resultb += 1;
+    printf("%d", resultb);
+
+  return 0;
 }
